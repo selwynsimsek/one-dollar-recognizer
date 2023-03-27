ceea3f98e8982ef66ae5aa3559e934651e37835c
Selwyn Simsek <selwyn.simsek@quantinuum.com>
2023-03-27 20:35:08 +0100

    Implement skeleton of  gesture recognizer

--- src/main.lisp
+++ src/main.lisp
@@ -2,4 +2,22 @@
   (:use :cl))
 (in-package :one-dollar-recognizer)
 
-;; blah blah blah.
+;;; step 1
+(defun resample (points))
+(defun path-length (a))
+
+;;; step 2
+(defun rotate-to-zero (points))
+(defun rotate-by (points theta))
+
+;;; step 3
+
+(defun scale-to-square (points size))
+(defun translate-to-origin (points))
+
+;;; step 4
+
+(defun recognize (points templates))
+(defun distance-at-best-angle (points template theta-a theta-b theta-delta))
+(defun distance-at-angle (points template theta))
+(defun path-distance (a b))
